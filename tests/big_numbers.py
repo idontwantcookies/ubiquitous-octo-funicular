471027dad8f6447096e736f87240a5ac2fe80785
Felipe Muniz <35815542+idontwantcookies@users.noreply.github.com>
2024-08-12 13:40:46 -0300

    Solve ambiguity with test folders

--- tests/big_numbers.py
+++ tests/big_numbers.py
@@ -1,24 +1,24 @@
 extgcd = []
-with open("test/extended_gcd.txt") as file:
+with open("big_numbers/extended_gcd.txt") as file:
     for line in file:
         extgcd.append([int(x) for x in line.split()])
 
 bsgs = []
-with open("test/bsgs.txt") as file:
+with open("big_numbers/bsgs.txt") as file:
     for line in file:
         bsgs.append([int(x) for x in line.split()])
 
 modular_inverse = []
-with open("test/inverso_modular.txt") as file:
+with open("big_numbers/inverso_modular.txt") as file:
     for line in file:
         modular_inverse.append([int(x) for x in line.split()])
 
 fastexp = []
-with open("test/exp_binaria.txt") as file:
+with open("big_numbers/exp_binaria.txt") as file:
     for line in file:
         fastexp.append([int(x) for x in line.split()])
 
 primes = []
-with open("test/primes.txt") as file:
+with open("big_numbers/primes.txt") as file:
     for line in file:
         primes.append([int(x) for x in line.split()])
